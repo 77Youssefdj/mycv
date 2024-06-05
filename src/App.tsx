44bc350ea3569e55c5bdd60f3386a1f7ab409521
you18ssef <youssefiop551@gmail.com>
2024-06-05 16:15:46 +0100

    first commit4

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="">
+    <div className="route">
      
       <Routes>
         <Route path='/' element={ <Cv />} />
